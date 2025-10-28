516569b264532f0f0afdf3c49fef0b742cfc6e47
Auler09 <mateus.a.c.pereira084@gmail.com>
2025-10-28 09:53:21 -0300

    Colisão da nave adicionada

--- Assets/Scripts/Nave.cs
+++ Assets/Scripts/Nave.cs
@@ -2,8 +2,14 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private Rigidbody2D _rigidbody;
 
     public float moveSpeed = 5.0f;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
     private void Update()
     {
         float moveDirection = 0f;
@@ -17,7 +23,7 @@ public class Player : MonoBehaviour
             moveDirection = 1.0f;
         }
 
-        transform.position += new Vector3(moveDirection, 0f, 0f) * moveSpeed * Time.deltaTime;
+        _rigidbody.linearVelocity = new Vector2(moveDirection * moveSpeed, 0f);
         
     }
 }
