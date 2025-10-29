f039410a50b2786fcaa629856d12dec2c3c1e103
Auler09 <mateus.a.c.pereira084@gmail.com>
2025-10-29 17:45:15 -0300

    Tiro adicionado

--- Assets/Scripts/Nave.cs
+++ Assets/Scripts/Nave.cs
@@ -2,9 +2,15 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public Tiros tirosPrefab;
+    
     private Rigidbody2D _rigidbody;
 
     public float moveSpeed = 5.0f;
+
+    public float fireRate = 0.25f;
+
+    private float _lastShootTime = 0f;
 
     private void Awake()
     {
@@ -23,7 +29,20 @@ public class Player : MonoBehaviour
             moveDirection = 1.0f;
         }
 
-        _rigidbody.linearVelocity = new Vector2(moveDirection * moveSpeed, 0f);
-        
+        _rigidbody.linearVelocity = new Vector2(moveDirection * moveSpeed, _rigidbody.linearVelocity.y);
+
+        if ((Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow) || Input.GetMouseButton(0)) && Time.time - _lastShootTime >= fireRate)
+        {
+            Shoot();
+            _lastShootTime = Time.time;
+        }
+
+    }
+    
+    private void Shoot()
+    {
+        Tiros tiros = Instantiate(this.tirosPrefab, this.transform.position, Quaternion.identity);
+        Physics2D.IgnoreCollision(tiros.GetComponent<Collider2D>(), GetComponent<Collider2D>());
+        tiros.Project(Vector2.up);
     }
 }
---
+++ Assets/Scripts/Tiros.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Tiros : MonoBehaviour
+{
+    public float speed = 200.0f;
+
+    public float maxLifetime = 4.0f;
+
+    private Rigidbody2D _rigidbody;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    public void Project(Vector2 direction)
+    {
+        _rigidbody.AddForce(direction * this.speed);
+
+        Destroy(this.gameObject, this.maxLifetime);
+    }
+
+
+}
